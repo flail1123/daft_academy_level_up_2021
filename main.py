276df7c90e4387114119d9581156ba38b17833dd
flail1123 <kmarkowski2000@outlook.com>
2021-04-17 12:41:41 +0200

    next commit I hope it works this time

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.counter = 0
 
 @app.get("/")
-def root():
+def root_view():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def hello_name_view(name: str):
+    return f"Hello {name}"
+
+
+@app.get("/counter")
+def counter():
+    app.counter += 1
+    return app.counter
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from main import app
 
@@ -8,3 +9,20 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
+
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
