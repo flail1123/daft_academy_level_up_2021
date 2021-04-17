6bde676cfb15f2ca703c736dfa9698073b91cf8b
flail1123 <kmarkowski2000@outlook.com>
2021-04-17 12:56:06 +0200

    task 1.2

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 app.counter = 0
+
 
 @app.get("/")
 def root_view():
     return {"message": "Hello world!"}
 
+
+@app.post("/method")
+def method(response: Response):
+    response.status_code = status.HTTP_201_CREATED
+    return {"method": "POST"}
+
+
+@app.get("/method")
+def method():
+    return {"method": "GET"}
+
+
+@app.put("/method")
+def method():
+    return {"method": "PUT"}
+
+
+@app.delete("/method")
+def method():
+    return {"method": "DELETE"}
+
+
+@app.options("/method")
+def method():
+    return {"method": "OPTIONS"}
+
+
+'''
 @app.get("/hello/{name}")
 async def hello_name_view(name: str):
     return f"Hello {name}"
@@ -16,3 +45,4 @@ async def hello_name_view(name: str):
 def counter():
     app.counter += 1
     return app.counter
+'''
