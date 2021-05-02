c333a03ed0cf3a8634160e238a70af860dc70276
flail1123 <kmarkowski2000@outlook.com>
2021-05-02 22:22:03 +0200

    3.3 v10

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def welcome_message(format, request):
 
 @app.get("/welcome_session")
 def welcome_session(response: Response, request: Request, format: str = "", ads_id: Optional[str] = Cookie(None)):
-    if ads_id != app.login_session_token and app.login_session_token != "":
+    if ads_id != app.login_session_token or app.login_session_token != "":
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
@@ -152,7 +152,7 @@ def welcome_session(response: Response, request: Request, format: str = "", ads_
 
 @app.get("/welcome_token")
 def welcome_token(response: Response, request: Request, format: str = "", token: str = ""):
-    if token != app.login_token_token and app.login_token_token != "":
+    if token != app.login_token_token or app.login_token_token != "":
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
