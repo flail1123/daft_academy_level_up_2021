9a3882476763e78cec99e6cfe520f89e96bfb230
flail1123 <kmarkowski2000@outlook.com>
2021-05-04 21:55:40 +0200

    3.5 v5

--- main.py
+++ main.py
@@ -107,16 +107,16 @@ def hello(request: Request):
 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    # correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
-    # correct_password = secrets.compare_digest(credentials.password, "swordfish")
-    # if not (correct_username and correct_password):
-    #    raise HTTPException(
-    #        status_code=status.HTTP_401_UNAUTHORIZED,
-    #        detail="Incorrect email or password",
-    #        headers={"WWW-Authenticate": "Basic"},
-    #    )
+    correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
+    correct_password = secrets.compare_digest(credentials.password, "swordfish")
+    if not (correct_username and correct_password):
+       raise HTTPException(
+           status_code=status.HTTP_401_UNAUTHORIZED,
+           detail="Incorrect email or password",
+           headers={"WWW-Authenticate": "Basic"},
+       )
     app.count += 1
-    return credentials.username + "+" + credentials.password #+ str(app.count)
+    return credentials.username + "+" + credentials.password + str(app.count)
 
 
 @app.post("/login_session")
