fce8c719957cd0906903d2e29a029850e930614a
flail1123 <kmarkowski2000@outlook.com>
2021-05-15 17:30:53 +0200

    new3

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import PositiveInt
+from sqlalchemy.orm import Session
 
+from . import crud, schemas
+from .database import get_db
 from .views import router as northwind_api_router
 
 app = FastAPI()
 
 app.include_router(northwind_api_router, tags=["northwind"])
+@app.get("/shippers/{shipper_id}", response_model=schemas.Shipper)
+async def get_shipper(shipper_id: PositiveInt, db: Session = Depends(get_db)):
+    db_shipper = crud.get_shipper(db, shipper_id)
+    if db_shipper is None:
+        raise HTTPException(status_code=404, detail="Shipper not found")
+    return db_shipper
